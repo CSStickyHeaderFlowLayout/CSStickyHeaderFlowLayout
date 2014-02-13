17fe091ff5f3e499c32eecc995b1066b85962f3c
James Tang <jamz@jamztang.com>
2014-02-13 21:49:54 +0800

    Add Grow Header example

---
+++ Project/CSStickyHeaderFlowLayoutDemo/CSGrowHeaderViewController.h
@@ -0,0 +1,13 @@
+//
+//  CSGrowHeaderViewController.h
+//  CSStickyHeaderFlowLayoutDemo
+//
+//  Created by James Tang on 13/2/14.
+//  Copyright (c) 2014 Jamz Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSGrowHeaderViewController : UICollectionViewController
+
+@end
