96546d6660f5db5a09f3c3008dba2cf2f4f70cd3
James Tang <j@jamztang.com>
2015-10-02 17:33:34 +0800

    Update Podfile.lock to 0.2.10

---
+++ Project/Pods/Target Support Files/CSStickyHeaderFlowLayout/CSStickyHeaderFlowLayout-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CSStickyHeaderFlowLayout : NSObject
+@end
+@implementation PodsDummy_CSStickyHeaderFlowLayout
+@end
--- Project/Pods/Target Support Files/Pods-CSStickyHeaderFlowLayout/Pods-CSStickyHeaderFlowLayout-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CSStickyHeaderFlowLayout : NSObject
-@end
-@implementation PodsDummy_Pods_CSStickyHeaderFlowLayout
-@end
